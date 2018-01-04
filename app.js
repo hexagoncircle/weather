7f2ae5a629579e5ad00bbfc40e6fe6202e9d05f0
Ryan Mulligan <rmulligan@elevatedthird.com>
2018-01-03 20:54:25 -0800

    Move weather js into separate file and create dynamic weather calls

--- app.js
+++ app.js
@@ -15,12 +15,17 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-// geocode.geocodeAddress(argv.address, (errorMsg, results) => {
-//   if (errorMsg) {
-//     console.log(errorMsg);
-//   } else {
-//     console.log(JSON.stringify(results, undefined, 2));
-//   }
-// });
-
-weather.getWeather();
+geocode.geocodeAddress(argv.address, (errorMsg, results) => {
+  if (errorMsg) {
+    console.log(errorMsg);
+  } else {
+    console.log(results.address);
+    weather.getWeather(results.latitude, results.longitude, (errorMsg, weatherResults) => {
+      if (errorMsg) {
+        console.log(errorMsg);
+      } else {
+        console.log(`It's currently ${weatherResults.temperature}, but feels like ${weatherResults.apparentTemperature}.`);
+      }
+    });
+  }
+});
--- weather/weather.js
+++ weather/weather.js
@@ -1,14 +1,18 @@
 const request = require('request');
+const FORECASTAPIKEY = 'dcda0a2772f65a89e1c8a7b90a278483';
 
-var getWeather = () => {
+var getWeather = (lat, lng, callback) => {
   request({
-    url: 'https://api.darksky.net/forecast/[key]/[lat]/[long]',
+    url: `https://api.darksky.net/forecast/dcda0a2772f65a89e1c8a7b90a278483/${lat},${lng}`,
     json: true,
   }, (error, response, body) => {
     if (!error && response.statusCode === 200) {
-      console.log(body.currently.temperature);    
+      callback(undefined, {
+        temperature: body.currently.temperature,
+        apparentTemperature: body.currently.apparentTemperature
+      });
     } else {
-      console.log('Unable to connect to Forecast.io server.');    
+      callback('Unable to connect to Forecast.io server.');    
     }
   });
 };
